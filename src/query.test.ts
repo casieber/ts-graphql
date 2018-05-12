5613a4ed8a031098e5559e6b77168e969537ffea
Colby Sieber <sieber.colby@gmail.com>
2018-05-12 16:59:42 -0700

    Added a few initial, basic tests

---
+++ src/query.test.ts
@@ -0,0 +1,74 @@
+import { parse } from 'graphql';
+
+import { documentToType } from './query';
+
+describe('documentToType()', () => {
+	test('works for single item', () => {
+		const result = documentToType(parse(`{ name }`));
+		expect(result).toEqual({ name: true });
+	});
+
+	test('works for multiple top level fields', () => {
+		const result = documentToType(parse(`{ name age height }`));
+		expect(result).toEqual({
+			name: true,
+			age: true,
+			height: true,
+		});
+	});
+
+	test('works for single deep fields', () => {
+		const result = documentToType(
+			parse(`{ this { is { a { deep { field } } } } }`),
+		);
+		expect(result).toEqual({
+			this: {
+				is: {
+					a: {
+						deep: {
+							field: true,
+						},
+					},
+				},
+			},
+		});
+	});
+
+	test('works for mixed field levels', () => {
+		const result = documentToType(
+			parse(`{
+				name {
+					first
+					last
+				}
+				age
+				posts {
+					title
+					comments {
+						text
+						author {
+							name
+						}
+					}
+				}
+			}`),
+		);
+
+		expect(result).toEqual({
+			name: {
+				first: true,
+				last: true,
+			},
+			age: true,
+			posts: {
+				title: true,
+				comments: {
+					text: true,
+					author: {
+						name: true,
+					},
+				},
+			},
+		});
+	});
+});
